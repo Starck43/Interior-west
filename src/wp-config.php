ecc1b4427ea305c19b5c52469691fad5780ffaa3
Starck43 <saloon.as@gmail.com>
2019-07-30 18:27:50 +0300

    Update config files

--- src/wp-config.php
+++ src/wp-config.php
@@ -16,11 +16,11 @@ define( 'WP_CONTENT_URL',  "//{$_SERVER['HTTP_HOST']}/wp-content" );
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'your_db_name');
+define('DB_NAME', 'wordpress');
 /** Имя пользователя MySQL */
-define('DB_USER', 'your_username');
+define('DB_USER', 'admin');
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'your_password');
+define('DB_PASSWORD', 'admin');
 /** Имя сервера MySQL (обычно не изменяется) */
 define('DB_HOST', 'localhost');
 /** Кодировка базы данных для создания таблиц. */
@@ -48,14 +48,14 @@ $table_prefix  = 'wp_';
  * Чтобы не сочинять ключи самому их можно быстро генерировать по следующей ссылке: https://api.wordpress.org/secret-key/1.1/salt/
  */
  
-define('AUTH_KEY',         'впишите сюда уникальную фразу');
-define('SECURE_AUTH_KEY',  'впишите сюда уникальную фразу');
-define('LOGGED_IN_KEY',    'впишите сюда уникальную фразу');
-define('NONCE_KEY',        'впишите сюда уникальную фразу');
-define('AUTH_SALT',        'впишите сюда уникальную фразу');
-define('SECURE_AUTH_SALT', 'впишите сюда уникальную фразу');
-define('LOGGED_IN_SALT',   'впишите сюда уникальную фразу');
-define('NONCE_SALT',       'впишите сюда уникальную фразу');
+define('AUTH_KEY',         'Svy:bUnP(>h1CZ0HWXR4d(~!{g4R^-}2x(2@+n>+/QT=+-.6m08|+YIZeNv2-Xd.');
+define('SECURE_AUTH_KEY',  'urB0Y(Mi+n@+dZOkrp+)bsuW<)rWg?fYy* rG-{dsoRkcZ=#k_O#Rb*&G+3PbbuB');
+define('LOGGED_IN_KEY',    'K!P ^IaIgfh#^1z/tC4jAg5[>#K>G+Km-p$lsM+X;[>3|%*i{+Sp:+oduZC=T-}Y');
+define('NONCE_KEY',        '@YKTH+5wNNY`mYGdz579flAR+-L.TUT#8E:OQJgZ<%eQ yTk[c61p=uP+J$,^S*U');
+define('AUTH_SALT',        '79TKa#vN<_ytQ<(%/oJVpav@s|K[NWnxY;Wf{C.bi7IAw|RlEZVLwZ&3(v?_2u@>');
+define('SECURE_AUTH_SALT', '3(s[s1$#KtIbhz4teh1[W|w8gk#!|k<9Ol60Q;Gq,,b XYyEyxhtPqF^Dv8SN0PF');
+define('LOGGED_IN_SALT',   '2kRQJ[K]d8mB)@_df:h/ke+sb$s$6Cy+?a3+>YLxTGrut]YTH%cVOUI7Hj6*iXP|');
+define('NONCE_SALT',       'G6LH: o!>%u4)^3>frEh h_(OwzeQ#gX:vXUvbc=EDl4vNh Gj_4a|F+ urtD{WH');
 
 /**
  * Для разработчиков: Режим отладки WordPress. В обычном режиме false
--- src/wp-content/themes/mytheme-child/functions.php
+++ src/wp-content/themes/mytheme-child/functions.php
@@ -4,17 +4,17 @@
 if ( !defined( 'ABSPATH' ) ) exit;
 
 //echo get_home_url( null, 'wp-admin/', 'https' ); //Example.com: https://example.com/wp-admin/
-$home_url = get_home_url(); //get_stylesheet_directory_uri(); for theme-child URL
-$main_css_file = $home_url . '/css/main.min.css';
-if ( !file_exists( $main_css_file ) $main_css_file = $home_url . '/css/main.css';
+$home_url = get_stylesheet_directory_uri(); // for theme-child URL //get_home_url();
+$main_css_file = '/main.min.css';
+if ( !file_exists( $main_css_file ) $main_css_file = '/main.css';
 
 
 // STYLES registry
-wp_enqueue_style( 'mytheme-styles', $main_css_file);
+wp_enqueue_style( 'mytheme-styles', $home_url . '/css' . $main_css_file);
 wp_enqueue_style( 'vendors-styles', $home_url . '/css/vendors.min.css');
 // !!! Check for dublicates of the styles below in css/vendors.css via @import
-//wp_enqueue_style( 'magnific-popup', $home_url . '/libs/magnific-popup/dist/magnific-popup.css');
-//wp_enqueue_style( 'font-awesome-styles', $home_url . '/libs/awesome/css/font-awesome.min.css');
+//wp_enqueue_style( 'magnific-popup', $home_url . '/plugins/magnific-popup/dist/magnific-popup.css');
+//wp_enqueue_style( 'font-awesome-styles', $home_url . '/plugins/awesome/css/font-awesome.min.css');
 
 // SCRIPTS registry
 add_action( 'mytheme_enqueue_scripts', 'mytheme_scripts_add' );
