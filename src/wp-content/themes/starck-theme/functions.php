0f3928159a14f4a6d928a41fe0b3496763d143e2
Starck43 <saloon.as@gmail.com>
2020-11-01 22:18:22 +0300

    Added yandex metrika and fixed minor issues

--- src/wp-content/themes/starck-theme/functions.php
+++ src/wp-content/themes/starck-theme/functions.php
@@ -1,5 +1,5 @@
 <?php
-define( 'STARCK_VERSION', '1.8.8' );
+define( 'STARCK_VERSION', '1.8.9' );
 
 //!-- START ENQUEUE PARENT ACTION
 
@@ -10,7 +10,7 @@ if ( !file_exists( dirname( __FILE__ ) . '/css/' . $main_css_file )) $main_css_f
 
 
 // STYLES registry
-if(!is_admin()) {
+if( !is_admin() ) {
 	wp_enqueue_style( 'starck-theme', $home_url . '/css/' . $main_css_file, array(), STARCK_VERSION);
 	wp_enqueue_style( 'vendors', $home_url . '/css/vendors.min.css', array(), STARCK_VERSION);
 }
@@ -38,35 +38,22 @@ function starck_scripts_add() {
 //!-- END ENQUEUE PARENT ACTION
 
 require_once get_template_directory() . '/inc/theme-functions.php'; // Include main theme functions
-require_once get_template_directory() . '/inc/meta-controls.php'; // Include custom meta in pages (i.e. header gallery or hide title)
+if ( is_admin() )
+	require_once get_template_directory() . '/inc/meta-controls.php'; // Include custom meta in pages (i.e. header gallery or hide title)
 //require_once get_template_directory() . '/inc/projects_layout.php'; // Include Projects Post Layout
+?>
 
-// Функция проверки ответа сервера Google reCAPTCHA v2 (checkbox method)
-/*
-function verify_recaptcha_response() {
-	$recaptcha_site_key = '6LcEq7wUAAAAAE9OJS3JV0Rdiei50YWtYglRHEqC'; // ключ сайта взят отсюда ---> https://www.google.com/recaptcha/admin/site/347839669/settings
-	$recaptcha_secret_key = '6LcEq7wUAAAAAL4XcOZMlLXFzOycVN9J_kXT9Ib7';  // секретный ключ взят отсюда ---> https://www.google.com/recaptcha/admin/
-	if ( isset ( $_POST['g-recaptcha-response'] ) ) {
-		$captcha_response = $_POST['g-recaptcha-response'];
-	} else {
-		return false;
-	}
-    // Verify the captcha response from Google
-	$response = wp_remote_post(
-		'https://www.google.com/recaptcha/api/siteverify',
-		array(
-			'body' => array(
-				'secret' => $recaptcha_secret_key,
-				'response' => $captcha_response
-			)
-		)
-	);
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-	$success = false;
-	if ( $response && is_array( $response ) ) {
-		$decoded_response = json_decode( $response['body'] );
-		$success = $decoded_response->success;
-	}
-	return $success;
-}
-*/
+   ym(51322627, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/51322627" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
